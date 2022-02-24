5f1299b8afbf2d43ca21945971eba1df2b334771
Francisco Pottinger-Marques <fpottinger.marques@gmail.com>
2022-02-24 15:30:09 +0000

    added files to refactor

---
+++ test/gilded_rose.test.js
@@ -0,0 +1,9 @@
+const {Shop, Item} = require("../src/gilded_rose");
+
+describe("Gilded Rose", () => {
+  it("should foo", () => {
+    const gildedRose = new Shop([new Item("foo", 0, 0)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("fixme");
+  });
+})
